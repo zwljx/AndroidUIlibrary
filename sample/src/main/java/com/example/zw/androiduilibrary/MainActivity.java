b4e5acf31a074c1d373cdb66fcfc46b63735a06a
lujinxin <lujinxin@lujinxindeMac-mini.local>
2016-04-30 17:46:55 +0800

    project init

---
+++ sample/src/main/java/com/example/zw/androiduilibrary/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.zw.androiduilibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"anniu",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
